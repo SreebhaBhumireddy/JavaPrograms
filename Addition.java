5d97c62478e6d687085c5286238bac902ed39ac9
SreebhaBhumireddy <209X1A0508@gprec.ac.in>
2022-06-08 10:43:05 +0530

    First java program

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+  public static void main(String[] args) {
+    System.out.println("Enter two numbers");
+    int a= 10;
+    int b = 20;
+    System.out.println(a + " " + b);
+    int sum = a + b;
+    System.out.println("The sum is: " + sum);
+  }
+}
